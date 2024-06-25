db7dd3edbbd3265344dd06fb76b98ad2d9db6de2
Ibrahim Abdulmojeed <ibraheemabdulmojeed1@gmail.com>
2024-06-25 09:17:18 +0100

    load all email conf from .env to allow for different email service on the server

--- base/settings.py
+++ base/settings.py
@@ -136,13 +136,12 @@ AUTH_USER_MODEL = "users.User"
 
 # Email settings
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-DEFAULT_FROM_EMAIL = "ibraheemabdulmojeed1@gmail.com"
-EMAIL_HOST = "smtp-relay.brevo.com"
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
-# EMAIL_USE_SSL = True
-EMAIL_PORT = 587
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_PORT = config('EMAIL_PORT')
 
 REST_FRAMEWORK = {
     # YOUR SETTINGS
